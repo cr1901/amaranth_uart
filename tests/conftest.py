14721cd8f81bb14e72819cfa92757a5cc44153ce
William D. Jones <thor0505@comcast.net>
2023-06-05 03:50:19 -0400

    Simplify sim_mod a bit, don't generate vcds by default.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,22 @@ import pytest
 from amaranth.sim import Simulator
 
 
-class SimulatorFixture:
-    def __init__(self, mod, clks, req):
-        self.name = req.node.name
-        self.sim = Simulator(mod.args[0])
+def pytest_addoption(parser):
+    parser.addoption(
+        "--vcds",
+        action="store_true",
+        help="generate Value Change Dump (vcds) from simulations",
+    )
 
-        for clk in clks.args[0]:
+
+class SimulatorFixture:
+    def __init__(self, req, cfg):
+        self.mod = req.node.get_closest_marker("module").args[0]
+        self.name = req.node.name
+        self.sim = Simulator(self.mod)
+        self.vcds = cfg.getoption("vcds")
+
+        for clk in req.node.get_closest_marker("clks").args[0]:
             self.sim.add_clock(clk)
 
     def run(self, sync_processes, processes=[]):
@@ -18,12 +28,14 @@ class SimulatorFixture:
         for p in processes:
             self.sim.add_process(p)
 
-        with self.sim.write_vcd(self.name + ".vcd", self.name + ".gtkw"):
+        if self.vcds:
+            with self.sim.write_vcd(self.name + ".vcd", self.name + ".gtkw"):
+                self.sim.run()
+        else:
             self.sim.run()
 
 
 @pytest.fixture
-def sim_mod(request):
-    mod = request.node.get_closest_marker("module")
-    clks = request.node.get_closest_marker("clks")
-    return (SimulatorFixture(mod, clks, request), mod.args[0])
+def sim_mod(request, pytestconfig):
+    simfix = SimulatorFixture(request, pytestconfig)
+    return (simfix, simfix.mod)
